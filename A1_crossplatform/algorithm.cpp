f1674299b1aaf8c0e4308f9bba5d4dad3b5a1cb9
hennieuni <hennieuni@gmail.com>
2024-09-03 19:39:03 +1200

    uc_explist added
    added uc_explist untested to the sample code while fixing minor syntax errors for

--- A1_crossplatform/algorithm.cpp
+++ A1_crossplatform/algorithm.cpp
@@ -1,11 +1,38 @@
 
-
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <iomanip>
+#include <fstream>
+#include <functional>
+#include <algorithm>
 #include "algorithm.h"
 
 using namespace std;
 
+const int PUZZLE_SIZE = 3;
+const int NUM_MOVES = 4;
 
+const vector<int> DX = {-1, 0, 1, 0};
+const vector<int> DY = {0, 1, 0, -1};
 
+struct Node {
+    string state;
+    string path;
+    int cost;
+    int heuristic;
+
+    Node(const string &s, const string &p, int c, int h)
+        : state(s), path(p), cost(c), heuristic(h) {}
+
+    bool operator>(const Node &other) const {
+        return (cost + heuristic) > (other.cost + other.heuristic);
+    }
+};
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -31,11 +58,47 @@ string uc_explist(string const initialState, string const goalState, int& pathLe
     // cout << "------------------------------" << endl;
 	actualRunningTime=0.0;	
 	startTime = clock();
-	srand(time(NULL)); //RANDOM NUMBER GENERATOR - ONLY FOR THIS DEMO.  YOU REALLY DON'T NEED THIS! DISABLE THIS STATEMENT.
+
+	/* these are for test to see if code works together
+    srand(time(NULL)); //RANDOM NUMBER GENERATOR - ONLY FOR THIS DEMO.  YOU REALLY DON'T NEED THIS! DISABLE THIS STATEMENT.
 	maxQLength= rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY.
 	numOfStateExpansions = rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY
+    */
 
+    priority_queue<Node, vector<Node>, greater<Node>> openQueue;
+    set<string> closedSet;
+    int numDeletions = 0;
 
+     openQueue.emplace(initialState, "", 0, 0);
+    maxQLength = 0;
+    numOfStateExpansions = 0;
+
+    while (!openQueue.empty()) {
+        maxQLength = max(maxQLength, (int)openQueue.size());
+
+        Node current = openQueue.top();
+        openQueue.pop();
+
+        if (current.state == goalState) {
+            pathLength = current.path.length();
+            actualRunningTime = float(clock() - startTime) / CLOCKS_PER_SEC;
+            return current.path;
+        }
+
+        if (closedSet.find(current.state) != closedSet.end()) {
+            continue;
+        }
+        closedSet.insert(current.state);
+        ++numOfStateExpansions;
+
+        vector<Node> successors = generateSuccessors(current);
+        for (auto &successor : successors) {
+            if (closedSet.find(successor.state) == closedSet.end()) {
+                openQueue.push(successor);
+            } else {
+                ++numDeletions;
+            }
+        }
 	
 	
 //***********************************************************************************************************
@@ -87,3 +150,26 @@ string aStar_ExpandedList(string const initialState, string const goalState, int
 		
 }
 
+vector<Node> generateSuccessors(const Node &node) {
+    std::vector<Node> successors;
+    int pos = node.state.find('0');
+    int x = pos / PUZZLE_SIZE;
+    int y = pos % PUZZLE_SIZE;
+
+    for (int i = 0; i < NUM_MOVES; ++i) {
+        int newX = x + DX[i];
+        int newY = y + DY[i];
+        if (isValid(newX, newY)) {
+            int newPos = newX * PUZZLE_SIZE + newY;
+            std::string newState = node.state;
+            std::swap(newState[pos], newState[newPos]);
+            std::string newPath = node.path + (i == 0 ? 'U' : (i == 1 ? 'R' : (i == 2 ? 'D' : 'L')));
+            successors.emplace_back(newState, newPath, node.cost + 1, 0);
+        }
+    }
+    return successors;
+}
+
+bool isValid(int x, int y) {
+    return (x >= 0 && x < PUZZLE_SIZE && y >= 0 && y < PUZZLE_SIZE);
+}
