103a331bd8efdb8e93c0f24db33e2833887ae8b8
hennieuni <hennieuni@gmail.com>
2024-09-06 18:30:20 +1200

    uc_search adjusted
    uc_search is almost complete just need to adjust q lenght to work

--- A1_crossplatform/algorithm.cpp
+++ A1_crossplatform/algorithm.cpp
@@ -1,15 +1,18 @@
-
 #include <iostream>
 #include <vector>
 #include <queue>
 #include <set>
+#include <unordered_map>
 #include <string>
-#include <cmath>
+#include <algorithm>
 #include <ctime>
+#include <cmath>
+#include <functional>
+
 #include <iomanip>
 #include <fstream>
-#include <functional>
-#include <algorithm>
+
+
 #include "algorithm.h"
 
 using namespace std;
@@ -20,17 +23,16 @@ const int NUM_MOVES = 4;
 const vector<int> DX = {0, 1, 0, -1};
 const vector<int> DY = {1, 0, -1, 0};
 
-struct Node {
+struct PuzzleState {
     string state;
     string path;
-    int cost;
-    int heuristic;
+    int g; // path cost (number of moves)
+    int h; // heuristic value
 
-    Node(const string &s, const string &p, int c, int h)
-        : state(s), path(p), cost(c), heuristic(h) {}
+    PuzzleState(string s, string p, int g_cost, int h_cost) : state(s), path(p), g(g_cost), h(h_cost) {}
 
-    bool operator>(const Node &other) const {
-        return (cost + heuristic) > (other.cost + other.heuristic);
+    bool operator<(const PuzzleState &other) const {
+        return (g + h) > (other.g + other.h); // min-heap
     }
 };
 
@@ -38,25 +40,33 @@ bool isValid(int x, int y) {
     return (x >= 0 && x < PUZZLE_SIZE && y >= 0 && y < PUZZLE_SIZE);
 }
 
-vector<Node> generateSuccessors(const Node &node) {
-    vector<Node> successors;
-    int pos = node.state.find('0');
-    int x = pos / PUZZLE_SIZE;
-    int y = pos % PUZZLE_SIZE;
+vector<pair<string, char>> get_successors(const string &state) {
+    vector<pair<string, char>> successors;
+    int zero_index = state.find('0');
 
-    for (int i = 0; i < NUM_MOVES; ++i) {
-        int newX = x + DX[i];
-        int newY = y + DY[i];
-        if (isValid(newX, newY)) {
-            int newPos = newX * PUZZLE_SIZE + newY;
-            string newState = node.state;
-            swap(newState[pos], newState[newPos]);
-            string newPath = node.path + (i == 0 ? 'R' : (i == 1 ? 'D' : (i == 2 ? 'L' : 'U')));
-            successors.emplace_back(newState, newPath, node.cost + 1, 0);
+    // Define possible moves and their directions (Up, Right, Down, Left)
+    static const vector<pair<int, char>> directions = {
+        {-3, 'U'}, {1, 'R'}, {3, 'D'}, {-1, 'L'}
+    };
+
+    // Check for valid moves based on the blank tile's position
+    for (const auto& [move, dir] : directions) {
+        int new_index = zero_index + move;
+        if (new_index >= 0 && new_index < 9) {
+            if ((zero_index % 3 == 0 && dir == 'L') || (zero_index % 3 == 2 && dir == 'R')) {
+                continue; // Invalid left or right move due to edge
+            }
+            string new_state = state;
+            swap(new_state[zero_index], new_state[new_index]);
+            successors.push_back({new_state, dir});
         }
     }
+
     return successors;
 }
+
+
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -68,60 +78,49 @@ vector<Node> generateSuccessors(const Node &node) {
 ////////////////////////////////////////////////////////////////////////////////////////////
 string uc_explist(string const initialState, string const goalState, int& pathLength, int &numOfStateExpansions, int& maxQLength,
                                float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap, int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions){
-											 
-   string path;
-   clock_t startTime;
-   
-   numOfDeletionsFromMiddleOfHeap=0;
-   numOfLocalLoopsAvoided=0;
-   numOfAttemptedNodeReExpansions=0;
-
-
-    // cout << "------------------------------" << endl;
-    // cout << "<<uc_explist>>" << endl;
-    // cout << "------------------------------" << endl;
-	actualRunningTime=0.0;	
-	startTime = clock();
-
-	/* these are for test to see if code works together
-    srand(time(NULL)); //RANDOM NUMBER GENERATOR - ONLY FOR THIS DEMO.  YOU REALLY DON'T NEED THIS! DISABLE THIS STATEMENT.
-	maxQLength= rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY.
-	numOfStateExpansions = rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY
-    */
-
-    priority_queue<Node, vector<Node>, greater<Node>> openQueue;
-    set<string> closedSet;
     
+    clock_t startTime = clock();;
+    priority_queue<PuzzleState> pq;
+    set<string> visited;
 
-    openQueue.emplace(initialState, "", 0, 0);
-    maxQLength = 0;
+    pq.push(PuzzleState(initialState, "", 0, 0));
+    visited.insert(initialState);
+    maxQLength = 1;
     numOfStateExpansions = 0;
+    numOfDeletionsFromMiddleOfHeap=0;
+    numOfLocalLoopsAvoided=0;
+    numOfAttemptedNodeReExpansions=0;
+	actualRunningTime=0.0;	
 
-    while (!openQueue.empty()) {
-        maxQLength = max(maxQLength, (int)openQueue.size());
+    string path;
 
-        Node current = openQueue.top();
-        openQueue.pop();
+    while (!pq.empty()) {
+        //maxQLength = max(maxQLength, (int)openQueue.size());
+
+        PuzzleState current = pq.top();
+        pq.pop();
 
         if (current.state == goalState) {
-            pathLength = current.path.length();
+            pathLength = current.g;
             actualRunningTime = float(clock() - startTime) / CLOCKS_PER_SEC;
             return current.path;
         }
 
+        /*
         if (closedSet.find(current.state) != closedSet.end()) {     //redundent
             continue;
         }
-        closedSet.insert(current.state);
-        ++numOfStateExpansions;
 
-        vector<Node> successors = generateSuccessors(current);
+        closedSet.insert(current.state);
+        */
         
-        for (auto &successor : successors) {
-            if (closedSet.find(successor.state) == closedSet.end()) {
-                openQueue.push(successor);
-            } else {
-                ++numOfDeletionsFromMiddleOfHeap;
+        ++numOfStateExpansions;
+        vector<pair<string, char>> successors = get_successors(current.state);
+        
+        for (const auto& [successor, direction] : successors) {
+            if (visited.find(successor) == visited.end()) {
+                visited.insert(successor);
+                pq.push(PuzzleState(successor, current.path + direction, current.g + 1, 0));
             }
         }
     }
